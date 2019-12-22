8cee0ae2abf2e2575bba9dd0ec2f69541d52346e
riih <ratealt@gmail.com>
2019-12-23 01:41:06 +0300

    Scrolling script

--- src/javascripts/scripts.js
+++ src/javascripts/scripts.js
@@ -3,3 +3,23 @@ import $ from 'jquery';
 
 window.jQuery = $;
 window.$ = $;
+
+let headerHeight = $('header').height();
+
+$(document).ready(() => {
+  headerHeight = $('header').height();
+});
+
+$(window).bind('resize', () => {
+  headerHeight = $('header').height();
+});
+
+$('a').click(function handle(e) {
+  e.preventDefault();
+  const elementClick = $(this).attr('href');
+  const destination = $(elementClick).offset().top - headerHeight;
+  $('html, body').animate({
+    scrollTop: destination,
+  }, 0);
+  return false;
+});
